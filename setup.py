831bc3c08dc51af3c318bfd08ea28d285a4e513d
Kevin McAreavey <kevinmcareavey@users.noreply.github.com>
2024-06-24 15:34:35 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="cuttlefish-api",
+    packages=["cuttlefish_api"],
+    version="0.0.2",
+    description="Cuttlefish API to integrate frontend and backend.",
+    author="Kevin McAreavey",
+    author_email="kevin.mcareavey@bristol.ac.uk",
+    license="Protected",
+    install_requires=[
+        "bjoern",
+        "dacite",
+        "falcon",
+        "falcon_auth",
+        "pendulum",
+    ],
+    extras_require={},
+    classifiers=[],
+    include_package_data=True,
+    platforms="any",
+)
